05c2533a03c1493c7ced3733bcbf45ed4717f4e3
Daniel Yamuza <hearthisinyourmind@gmail.com>
2019-07-28 17:55:49 +0200

    Updates

---
+++ precache-manifest.93f1295d219d502a4f45cdd752e1507f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17d9ba0740cf908fd3f4f3c1bf65b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d6f6ad0f4bd592cb63",
+    "url": "/static/css/main.13457565.chunk.css"
+  },
+  {
+    "revision": "fd9d802db4a797707e40",
+    "url": "/static/js/2.e907d1e5.chunk.js"
+  },
+  {
+    "revision": "d1d6f6ad0f4bd592cb63",
+    "url": "/static/js/main.0c177ed8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
